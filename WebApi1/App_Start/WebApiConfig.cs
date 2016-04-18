06dbb2c4899e6ccd1116a2c9613f8659fcd77279
ken(呂冠宏) <ken@edetw.com>
2016-04-18 14:45:00 +0800

    將ActionFilter註冊到全站

--- WebApi1/App_Start/WebApiConfig.cs
+++ WebApi1/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebApi1.Controllers;
 
 namespace WebApi1
 {
@@ -9,6 +10,7 @@ namespace WebApi1
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Filters.Add(new 錯誤捕捉Attribute());
             // Web API 設定和服務
 
             // Web API 路由
