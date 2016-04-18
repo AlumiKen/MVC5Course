d39eb1a1eff6b8aa7e65bb6e04ec3f2f1afa4d04
ken(呂冠宏) <ken@edetw.com>
2016-04-18 14:41:09 +0800

    webapi加入模型驗證

---
+++ WebApi1/Controllers/錯誤捕捉Attribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.ModelBinding;
+using System.Linq;
+
+namespace WebApi1.Controllers
+{
+    public class 錯誤捕捉Attribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ModelState.IsValid == false)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+        }
+    }
+}
